96c13e08dcedb23416b3dc5547592ddc684fc760
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-21 08:04:36 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About(props) {
       <h2>About Me</h2>
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <h2>{props.links}</h2>
+      <h3>{props.links}</h3>
       <a href={props.github}>{props.github}</a>
       <a href={props.linkedin}>{props.linkedin}</a>
     </div>
