41d13d5114f2da7b23458ad9e8457de7492512d8
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-27 03:47:41 +0300

    adding links file

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio !== '' && props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <h3>{props.links}</h3>
       <a href={props.github}>{props.github}</a>
